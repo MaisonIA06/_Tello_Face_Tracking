30bef5694c17185aa96ef5b23bf463d34c5aafce
wnaiji <wnaiji@maison-intelligence-artificielle.com>
2025-11-18 13:35:04 +0100

    cleaning

--- scripts/get_dataset.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Donwloading dataset ..."
-
-curl -L "https://universe.roboflow.com/ds/YlLRkDOFdS?key=$1" > roboflow.zip; unzip roboflow.zip -d datasets; rm roboflow.zip
-
